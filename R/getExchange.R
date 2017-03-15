095ae2131fe752dbecb1d73a1b724b42a6ca0848
Yuanchao Xu <xuyuanchao37@gmail.com>
2017-03-15 19:25:51 +0800

    add getexchange

--- R/getExchange.R
+++ R/getExchange.R
@@ -1,3 +1,19 @@
+#' get a company's listed location
+#'
+#' @param tickers ticker/sympol of a company, TICKERS MUST BE CHARACTERs, '006027' INSTEAD OF '6027'
+#' @details
+#' Data comes from www.finance.sina.com.cn
+#' @references
+#' www.finance.sina.com.cn
+#' @export
+#' @return A data table with a listed companies' ticker, security name and listed exchange location
+#' @examples
+#' \dontrun{
+#' getExchange('600601')
+#' getExchange(c('00005', '00001'))
+#' }
+
+
 getExchange <- function(tickers) {
 
   # this is the way to get data from hexun
@@ -22,14 +38,48 @@ getExchange <- function(tickers) {
   url2 <- '&name=suggestdata_1489564633542'
 
 
+  # counter
+  n <- 1
+
   for (i in tickers) {
     url <- paste(url1, i, url2, sep = '')
     a <- GET(url)
     b <- content(a, as = 'text')
+    c <- strsplit(b, ',')
 
+    index <- which (c[[1]] == i)
+    if (length(index) == 0) {
+      warning(paste('found nothing for', i))
+      res <- c(i, 'noInfo', 'noInfo')
+    } else {
+      name1 <- c[[1]][index[1] + 1]
+      name2 <- c[[1]][index[1]]
+
+      # get difference
+      loc <- Reduce(setdiff, strsplit(c(name1, name2), split = ""))
+      # paste them together
+      loc <- paste(loc, sep = '', collapse = '')
+
+      if (loc == '' & nchar(c[[1]][index[1] + 1]) == 5) {
+        loc <- 'hk'
+      }
+
+      info <- c(i, c[[1]][index[1] + 2], loc)
+    }
+    if (n == 1) {
+      res <- info
+    } else {
+      res <- rbind(res, info)
+    }
+
+    n <- n + 1
   }
 
-
+  # if the res only has one row, dimension has be to assigned
+  if (is.null(dim(res))) dim(res) <- c(1, 3)
+  res <- data.table(res)
+  colnames(res) <- c('Tickers', 'Name', 'location')
+  return(res)
 
 }
 
